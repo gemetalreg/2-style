a997dcf28c6919f671e13f09c6a05b7e8f882ffc
Andrei <itunereg@gmail.com>
2026-03-01 12:23:12 +0300

    fix

--- src/button/Button.jsx
+++ src/button/Button.jsx
@@ -2,7 +2,7 @@ import styles from  "./Button.module.css";
 
 
 function Button() {
-    return <button className={styles["button"]}> onClick={(e) => {e}}>Искать</button>
+    return <button className={styles["button"]} onClick={(e) => {e}}>Искать</button>
 }
 
 export default Button;
